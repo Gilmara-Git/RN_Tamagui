c594cca2ce8a05d867911bea7bed0b2fc3be8994
Gilmara Pimentel <gilmarapq@hotmail.com>
2024-06-20 09:37:29 -0400

    refactor: adjust Bear component style

--- src/components/Bear.tsx
+++ src/components/Bear.tsx
@@ -1,48 +1,42 @@
-import { XStack, Avatar, YStack, Separator , Paragraph,  } from 'tamagui';
+import { XStack, Avatar, YStack, Separator, Paragraph } from "tamagui";
+import { BearType } from "../store/zustand.store";
 
-export const Bear = ()=>{
+type TBear = {
+  data: BearType;
+};
 
-        return (
-            <YStack ai='center' jc='center' mt={20}> 
-                        <Avatar circular size='$6' zIndex={1}>
-                        <Avatar.Image
-                            accessibilityLabel='Nate Wienert'
-                            src='https://tsh.io/wp-content/uploads/2023/02/zustand-react-small.png'
-                        />
-                        <Avatar.Fallback delayMs={600} backgroundColor='$gray8'/>
-                    </Avatar>
-                <XStack mt={-30} jc='center' ai='center'>
-                    
-                    <YStack jc='center' gap='$1' bg='$red9' borderRadius={8} p={6}>
-                    
-                        <Paragraph 
-                            color='$gray3'
-                            fontSize={'$3'}
-                            fontFamily={'$body'}
-                            fontWeight={'700'}
-                            textAlign='center'
-                            paddingTop={25}
-                        
-                            >
-                                I am a BEAR component managed by ZUSTAND and  styled with TAMAGUI. 
-                                
-                                
-                        </Paragraph>
-                        <Separator marginVertical={5}/>
+export const Bear = ({ data }: TBear): JSX.Element => {
+  return (
+    <YStack ai="center" jc="center" mt={20}>
+      <Avatar circular size="$6" zIndex={1}>
+        <Avatar.Image accessibilityLabel="Nate Wienert" src={data.imageUrl} />
+        <Avatar.Fallback delayMs={600} backgroundColor="$gray8" />
+      </Avatar>
+      <XStack mt={-30} jc="center" ai="center">
+        <YStack jc="center" gap="$1" bg="$red9" borderRadius={8} p={6}>
+          <Paragraph
+            color="$gray3"
+            fontSize={"$3"}
+            fontFamily={"$body"}
+            // fontWeight={'700'}
+            textAlign="center"
+            paddingTop={25}
+          >
+            {data.description}
+          </Paragraph>
+          <Separator marginVertical={5} />
 
-                        <Paragraph
-                        color='$gray3'
-                        fontSize={'$3'}
-                        fontFamily={'$body'}
-                        // fontWeight={'600'}
-                        textAlign='center'
-                        >
-                        My creator, Gilmara Pimentel is keeping up with new TECHNOLOGIES.
-                        </Paragraph>
-
-                    </YStack>
-                    
-                </XStack>
-            </YStack>
-        )
-}
+          <Paragraph
+            color="$gray3"
+            fontSize={"$3"}
+            fontFamily={"$heading"}
+            // fontWeight={'400'}
+            textAlign="center"
+          >
+            {data.signature}
+          </Paragraph>
+        </YStack>
+      </XStack>
+    </YStack>
+  );
+};
