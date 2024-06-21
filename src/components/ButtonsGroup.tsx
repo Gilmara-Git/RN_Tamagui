ac5eb0a262aa0ada39c01ff59093ae062aaa0ab8
Gilmara Pimentel <gilmarapq@hotmail.com>
2024-06-21 16:42:54 -0400

    refactor: add circle to show list length

--- src/components/ButtonsGroup.tsx
+++ src/components/ButtonsGroup.tsx
@@ -1,5 +1,6 @@
 import { PlusCircle, MinusCircle } from '@tamagui/lucide-icons';
-import { XStack, Button, ButtonProps} from 'tamagui'
+import { XStack, Button, ButtonProps, View, Text, Circle } from 'tamagui';
+import { useBearStore } from '../store/zustand.store';
 
 type ButtonsGroupProps = ButtonProps & {
     updateBear : (action: string) => void;
@@ -7,15 +8,21 @@ type ButtonsGroupProps = ButtonProps & {
 
 export const ButtonsGroup = ({ updateBear, ...rest}: ButtonsGroupProps) : JSX.Element =>{
 
+  const bearsList = useBearStore((state) => state.bearsSelection);
+
     return (
         <XStack ai='center' jc='center' mt={12} gap={8}>
-        <Button bg='$red9' w='$2' h='$2' onPress={()=>updateBear('add')} {...rest} >
+        <Button pressStyle={{bg: '$red8'}} bg='$red9' size='$1' circular onPress={()=>updateBear('add')} {...rest} >
             <PlusCircle/>
         </Button>
+       <Circle bg='$red8' size='$2' borderWidth={2}>
+        <Text>{bearsList.length}</Text>
+       </Circle>
 
-        <Button bg='$red9' w='$2' h='$2' onPress={()=>updateBear('remove')} {...rest}>
+        <Button pressStyle={{bg: '$red8'}}  disabled={bearsList.length === 0} bg={bearsList.length !== 0 ? '$red9': '$gray10'} size='$1' circular onPress={()=>updateBear('remove')} {...rest}>
             <MinusCircle/>
         </Button>
+        
         </XStack>
 
     )
