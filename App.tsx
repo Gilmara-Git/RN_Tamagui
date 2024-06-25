394eec73876797264d52a988e530b58147f83bb7
Gilmara Pimentel <gilmarapq@hotmail.com>
2024-06-25 08:32:30 -0400

    refactor: use darkTheme state from zustand

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import { StatusBar } from "react-native";
 
+
 import {
   useFonts,
   Montserrat_400Regular,
@@ -13,30 +14,31 @@ import tamaguiConfig from "./tamagui.config";
 
 import { Routes } from "./src/routes";
 
+
 import { useThemeStore } from "./src/store/themeZustandStore";
 
 export default function App() {
-  const darkTheme = useThemeStore((state) => state.isDarkTheme);
 
+  const darkTheme = useThemeStore(state=> state.isDarkTheme);
   // const fontsLoaded = useFonts({
-  //   Inter: require("@tamagui/font-inter/otf/Inter-Medium.otf"),
-  //   InterBold: require("@tamagui/font-inter/otf/Inter-Bold.otf"),
-  // });
-
-  const [fontsLoaded, fontError] = useFonts({
-    Montserrat_400Regular,
-    Montserrat_600SemiBold,
-    Montserrat_700Bold,
-  });
-
-  if (!fontsLoaded && fontError) {
-    return null;
-  }
-
-  // if (!fontsLoaded) {
-  //   console.log(fontsLoaded);
-  //   return null;
-  // }
+    //   Inter: require("@tamagui/font-inter/otf/Inter-Medium.otf"),
+    //   InterBold: require("@tamagui/font-inter/otf/Inter-Bold.otf"),
+    // });
+    
+    const [fontsLoaded, fontError] = useFonts({
+      Montserrat_400Regular,
+      Montserrat_600SemiBold,
+      Montserrat_700Bold,
+    });
+    
+    if (!fontsLoaded && fontError) {
+      return null;
+    }
+    
+    // if (!fontsLoaded) {
+      //   console.log(fontsLoaded);
+      //   return null;
+      // }
 
   return (
     <TamaguiProvider config={tamaguiConfig}>
