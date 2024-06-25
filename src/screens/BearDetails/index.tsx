5e3cc9e2ddf5b8b1d00e84930df990ecf6fce1c1
Gilmara Pimentel <gilmarapq@hotmail.com>
2024-06-25 08:32:55 -0400

    feat: add BearDetails screen

--- src/screens/BearDetails/index.tsx
+++ src/screens/BearDetails/index.tsx
@@ -1,7 +1,11 @@
 import { Alert } from "react-native";
-import { YStack, Text, useCurrentColor } from "tamagui";
+import { YStack, Text, Image, Circle, XStack, Button } from "tamagui";
 import { useBearStore } from "../../store/zustand.store";
+import { useThemeStore } from "../../store/themeZustandStore";
 import { useRoute, useNavigation } from "@react-navigation/native";
+import { Theme } from "tamagui";
+import { Linking } from "react-native";
+import { Linkedin } from "@tamagui/lucide-icons";
 
 type BearDetailsParams = {
   id: string;
@@ -15,7 +19,8 @@ export const BearDetails = () => {
   const bearsList = useBearStore((state) => state.bearsSelection);
   const selectedBear = bearsList.find((bear) => bear.id === id);
 
-  console.log(id, selectedBear);
+  const darkTheme = useThemeStore((state) => state.isDarkTheme);
+  const bearName = selectedBear?.description.split(" ")[3];
 
   if (selectedBear === undefined) {
     setTimeout(() => {
@@ -34,9 +39,41 @@ export const BearDetails = () => {
     return;
   }
 
+
+const navigateLinkedIn = ()=>{
+
+  Linking.openURL('https://www.linkedin.com/in/gilmara-pimentel/')
+};
+
   return (
-    <YStack bg="$red9" ai="center" jc="center" flex={1}>
-      <Text>{selectedBear.signature}</Text>
-    </YStack>
+    <Theme name={darkTheme ? "light" : "dark"}>
+      <YStack bg="$red9" ai="center" jc="center" flex={1} gap="$10">
+        <Circle size={220} elevation="$4" backgroundColor="$accentColor">
+          <Image
+            source={{
+              uri: selectedBear.imageUrl,
+              width: 150,
+              height: 150,
+            }}
+          />
+        </Circle>
+
+        <YStack paddingHorizontal={60} jc="center">
+          <Text textAlign="center" fontSize="$4">
+            Hi, I am {bearName}
+          </Text>
+          <Text fontSize="$4" textAlign="center">
+            {selectedBear.signature}{" "}
+          </Text>
+        </YStack>
+        <Button
+          onPress={()=>navigateLinkedIn()}
+          backgroundColor="$accentColor"
+          pressStyle={{ backgroundColor: darkTheme ? "$blue9" : "$gray8" }}
+        >
+          <Linkedin size="$3" color={darkTheme ? "white" : "gray"} />
+        </Button>
+      </YStack>
+    </Theme>
   );
 };
