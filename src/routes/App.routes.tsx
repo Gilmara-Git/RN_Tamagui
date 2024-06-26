48a1d92718a625bbbf0c05f053e697745b48ec03
Gilmara Pimentel <gilmarapq@hotmail.com>
2024-06-25 21:45:03 -0400

    refactor: adjust header color and back icon

--- src/routes/App.routes.tsx
+++ src/routes/App.routes.tsx
@@ -4,6 +4,9 @@ import {
 } from "@react-navigation/native-stack";
 import { Home } from "../screens/Home";
 import { BearDetails } from "../screens/BearDetails";
+import { StepBack } from "@tamagui/lucide-icons";
+import { useNavigation } from "@react-navigation/native";
+import { useThemeStore } from "../store/themeZustandStore";
 
 type AppRoutesType = {
   home: undefined;
@@ -16,14 +19,30 @@ export type AppRoutesNavigationProps = NativeStackNavigationProp<AppRoutesType>;
 
 export const AppRoutes = (): JSX.Element => {
   const { Navigator, Screen } = createNativeStackNavigator<AppRoutesType>();
+  const navigate = useNavigation();
+  const darkTheme = useThemeStore((state) => state.isDarkTheme);
 
   return (
-    <Navigator>
-      <Screen name="home" component={Home} options={{ headerShown: false }} />
+    <Navigator screenOptions={{ headerShown: false }}>
+      <Screen name="home" component={Home} />
       <Screen
         name="bearDetails"
         component={BearDetails}
-        options={{ headerShown: false }}
+        options={{
+          headerShown: true,
+          title: "Details",
+          headerLeft: () => (
+            <StepBack
+              color={darkTheme ? "#000000" : "#CECECE"}
+              size="$1"
+              onPress={() => navigate.goBack()}
+            />
+          ),
+          headerStyle: { backgroundColor: "#e5484d" },
+          headerTintColor: darkTheme ? "#000000" : "#CECECE",
+          headerTransparent: true,
+          headerTitleAlign: "center",
+        }}
       />
     </Navigator>
   );
