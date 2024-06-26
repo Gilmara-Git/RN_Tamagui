df1764526418b64c335ae49e8da962861e9456fd
Gilmara Pimentel <gilmarapq@hotmail.com>
2024-06-25 21:45:21 -0400

    style: formatted with prettier

--- src/components/Bear.tsx
+++ src/components/Bear.tsx
@@ -1,51 +1,45 @@
-import { XStack, Avatar, YStack, Separator, Paragraph, styled } from "tamagui";
-import { TouchableOpacity, TouchableOpacityProps} from 'react-native';
+import { XStack, Avatar, YStack, Separator, Paragraph } from "tamagui";
+import { TouchableOpacity, TouchableOpacityProps } from "react-native";
 import { BearType } from "../store/zustand.store";
 
-type TBear = TouchableOpacityProps &{
+type TBear = TouchableOpacityProps & {
   data: BearType;
 };
 
-
 export const Bear = ({ data, ...rest }: TBear): JSX.Element => {
   return (
-    <TouchableOpacity 
-    activeOpacity={0.7}
-    {...rest}
-    >
+    <TouchableOpacity activeOpacity={0.7} {...rest}>
+      <YStack ai="center" jc="center" mt={20} p={2}>
+        <Avatar circular size="$6" zIndex={1}>
+          <Avatar.Image accessibilityLabel="Nate Wienert" src={data.imageUrl} />
+          <Avatar.Fallback delayMs={600} backgroundColor="$gray8" />
+        </Avatar>
+        <XStack mt={-30} jc="center" ai="center">
+          <YStack jc="center" gap="$1" bg="$red9" borderRadius={8} p={6}>
+            <Paragraph
+              color="$gray3"
+              fontSize={"$3"}
+              fontFamily={"$body"}
+              // fontWeight={'700'}
+              textAlign="center"
+              paddingTop={25}
+            >
+              {data.description}
+            </Paragraph>
+            <Separator marginVertical={5} />
 
-   
-    <YStack ai="center" jc="center" mt={20} p={2}>
-      <Avatar circular size="$6" zIndex={1}>
-        <Avatar.Image accessibilityLabel="Nate Wienert" src={data.imageUrl} />
-        <Avatar.Fallback delayMs={600} backgroundColor="$gray8" />
-      </Avatar>
-      <XStack mt={-30} jc="center" ai="center">
-        <YStack jc="center" gap="$1" bg="$red9" borderRadius={8} p={6}>
-          <Paragraph
-            color="$gray3"
-            fontSize={"$3"}
-            fontFamily={"$body"}
-            // fontWeight={'700'}
-            textAlign="center"
-            paddingTop={25}
-          >
-            {data.description}
-          </Paragraph>
-          <Separator marginVertical={5} />
-
-          <Paragraph
-            color="$gray3"
-            fontSize={"$3"}
-            fontFamily={"$heading"}
-            // fontWeight={'400'}
-            textAlign="center"
-          >
-            {data.signature}
-          </Paragraph>
-        </YStack>
-      </XStack>
-    </YStack>
+            <Paragraph
+              color="$gray3"
+              fontSize={"$3"}
+              fontFamily={"$heading"}
+              // fontWeight={'400'}
+              textAlign="center"
+            >
+              {data.signature}
+            </Paragraph>
+          </YStack>
+        </XStack>
+      </YStack>
     </TouchableOpacity>
   );
 };
--- src/components/ChangeTheme.tsx
+++ src/components/ChangeTheme.tsx
@@ -1,17 +1,14 @@
-import { XStack, Switch , SwitchProps  } from 'tamagui';
-import { Moon , Sun } from '@tamagui/lucide-icons';
+import { XStack, Switch, SwitchProps } from "tamagui";
+import { Moon, Sun } from "@tamagui/lucide-icons";
 
-export const ChangeTheme = ( { ...rest }: SwitchProps)=>{
-
-
-    return (
-        <XStack ai='center'gap='$3'>
-            <Sun size='$2'/>
-            <Switch size='$2' bg='$yellow8' native='mobile' {...rest}>
-                <Switch.Thumb animation='bouncy'/>
-            </Switch>
-            <Moon size='$2'/>
-
-        </XStack>
-    )
+export const ChangeTheme = ({ ...rest }: SwitchProps) => {
+  return (
+    <XStack ai="center" gap="$3">
+      <Sun size="$2" />
+      <Switch size="$2" native="mobile" {...rest}>
+        <Switch.Thumb animation="bouncy" />
+      </Switch>
+      <Moon size="$2" />
+    </XStack>
+  );
 };
